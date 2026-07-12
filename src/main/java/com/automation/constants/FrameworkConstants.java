94e7d75b66690c6da0ba0ba3b964a77f63ca0004
Your Name <you@example.com>
2026-07-13 02:09:02 +0530

    Add Project Structure, framework constants and property utility

---
+++ src/main/java/com/automation/constants/FrameworkConstants.java
@@ -0,0 +1,23 @@
+package com.automation.constants;
+
+public final class FrameworkConstants {
+
+    private FrameworkConstants(){
+
+    }
+
+    public static final String CONFIG_GILE_PATH = "src/test/resources/config.properties";
+    public static final String SCREENSHOT_PATH =
+            "screenshots/";
+
+    public static final String REPORT_PATH =
+            "reports/";
+
+    public static final String LOG_PATH =
+            "logs/";
+
+    public static final int EXPLICIT_WAIT = 10;
+
+    public static final int POLLING_INTERVAL = 500;
+
+}
---
+++ src/main/java/com/automation/utils/PropertyUtils.java
@@ -0,0 +1,42 @@
+package com.automation.utils;
+
+import com.automation.constants.FrameworkConstants;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Utility class to read values from config.properties.
+ */
+public class PropertyUtils {
+
+    private static final Properties properties = new Properties();
+
+    static{
+        try(FileInputStream file = new FileInputStream(
+                FrameworkConstants.CONFIG_GILE_PATH)){
+
+            properties.load(file);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    // Prevent object creation
+    private PropertyUtils(){
+
+    }
+
+    /**
+     * Returns the value associated with the given key.
+     *
+     * @param key Property key
+     * @return Property value
+     */
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+}
